72c656b5872d2d246f5ce110872c23e8a4bcc053
Aamir Sofi <eraamirsofi@gmail.com>
2022-09-01 16:06:07 +0530

    Prepared Category Data

--- components/Categories.js
+++ components/Categories.js
@@ -1,109 +1,117 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import CategoryCard from "./CategoryCard";
+import reactDom from "react-dom";
 
 const Categories = () => {
+	const arrCardData = [
+		{
+			uri: "https://picsum.photos/id/27/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/25/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/37/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/45/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/56/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/76/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/79/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/676/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/665/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/344/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/73/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/722/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/223/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/278/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/478/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/52/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/678/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/718/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/234/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/621/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/54/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/65/100/100",
+			title: "Hello",
+		},
+		{
+			uri: "https://picsum.photos/id/98/100/100",
+			title: "Hello",
+		},
+	];
 	return (
 		<ScrollView
 			contentContainerStyle={{
 				paddingHorizontal: 15,
 				paddingTop: 10,
+				paddingBottom: 10,
 			}}
 			horizontal
-			showsHorizontalScrollIndicator={false}>
+			showsHorizontalScrollIndicator={false}
+			className="bg-yellow-100">
 			{/* CatagoryCard */}
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/27/20/20"
-				title="hello 1"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/25/20/20"
-				title="hello 2"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/37/20/20"
-				title="hello 3"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/45/20/20"
-				title="hello 4"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/56/20/20"
-				title="hello 6"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/76/20/20"
-				title="hello 7"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
-			<CategoryCard
-				imgUrl="https://picsum.photos/id/78/20/20"
-				title="hello 8"
-			/>
+			{arrCardData.map((e) => {
+				return <CategoryCard key={e.uri} imgUrl={e.uri} title={e.title} />;
+			})}
 		</ScrollView>
 	);
 };
--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -2,19 +2,19 @@ import { Text, ScrollView, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
 
 const CategoryCard = ({ imgUrl, title }) => {
-  return (
-    <TouchableOpacity>
-      <Image
-        source={{
-          uri: imgUrl,
-        }}
-        className="h-20 w-20 rounded mx-2"
-      />
-      <Text className="absolute bottom-1 left-3 text-white font-bold">
-        {title}
-      </Text>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity>
+			<Image
+				source={{
+					uri: imgUrl,
+				}}
+				className="h-40 w-40 rounded mx-2"
+			/>
+			<Text className="absolute bottom-1 left-3 text-white font-bold">
+				{title}
+			</Text>
+		</TouchableOpacity>
+	);
 };
 
 export default CategoryCard;
