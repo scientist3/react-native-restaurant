c6a20ea313df1c6856fb67183058cc8bf90b7a9b
Aamir Sofi <eraamirsofi@gmail.com>
2022-09-01 12:01:05 +0530

    Intial setup for tailwind navigation

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import HomeScreen from "./screens/HomeScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const HomeScreen = () => {
+	return (
+		<View>
+			<Text className="text-red-500">HomeScreen loremx3</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => navigation.navigate("Details")}
+			/>
+		</View>
+	);
+};
+
+function DetailsScreen({ navigation }) {
+	return (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>Details Screen</Text>
+			<Button
+				title="Go to Details... again"
+				onPress={() => navigation.push("Details")}
+			/>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+	return (
+		// <View>Hello</View>
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
