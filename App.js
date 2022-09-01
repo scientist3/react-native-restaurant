be2ca8fb77ce5e4ebcd9f84cc9df512997cc83cd
Aamir Bashir <ksaak33@gmail.com>
2022-09-01 13:08:40 +0530

    Fixed the issue of new added HomeScreen Component

--- App.js
+++ App.js
@@ -3,42 +3,19 @@ import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import HomeScreen from "./screens/HomeScreen";
-
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text className="text-red-500">HomeScreen loremx3</Text>
-      {/* <Button
-				title="Go to Details"
-				onPress={() => navigation.navigate("Details")}
-			/> */}
-    </View>
-  );
-};
-
-// function DetailsScreen({ navigation }) {
-// 	return (
-// 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-// 			<Text>Details Screen</Text>
-// 			<Button
-// 				title="Go to Details... again"
-// 				onPress={() => navigation.push("Details")}
-// 			/>
-// 		</View>
-// 	);
-// }
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // <View>Hello</View>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
-      </Stack.Navigator>
+      <TailwindProvider>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+        </Stack.Navigator>
+      </TailwindProvider>
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,11 +2,15 @@ import { View, Text } from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
-	return (
-		<View>
-			<Text className="text-red-500">HomeScreen loremx3</Text>
-		</View>
-	);
+  return (
+    <View>
+      <Text className="text-red-500">HomeScreen loremx3, Hey Manisha</Text>
+      {/* <Button
+					title="Go to Details"
+					onPress={() => navigation.navigate("Details")}
+				/> */}
+    </View>
+  );
 };
 
 export default HomeScreen;
